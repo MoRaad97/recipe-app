7f925ebf7d27536d43a4f6ead76dc11ad30bfcbb
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-10-18 14:53:04 +0100

    Modify user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
 
   # GET /users or /users.json
   def index
@@ -7,7 +6,9 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1 or /users/1.json
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 
   # GET /users/new
   def new
