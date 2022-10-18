c1d86694145ecc4e1bd79e6c871bbdfa669dfa3c
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-10-18 18:55:34 +0300

    fix user register action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  root 'users#index'
   devise_for :users
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
