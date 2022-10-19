c284ca12bbfe0fc60e66b0d47ddf4bfa28394885
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-10-19 14:01:20 +0100

    Add delete button to card partial

--- db/migrate/20221019115424_create_recipes.rb
+++ db/migrate/20221019115424_create_recipes.rb
@@ -1,7 +1,6 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      create_table :recipes do |t|
       t.string :name
       t.string :preparation_time
       t.string :cooking_time
