b8de087d7ded177b6d1c2ff8f04d59c6a86d9881
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-10-20 19:04:12 +0300

    Update the models and Add new recipe_food model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,4 @@
 class Recipe < ApplicationRecord
-  # belongs_to :user
-
-  # validates :name, :preparation_time, :cooking_time, :description, presence: true
+  belongs_to :user
+  has_many :recipe_foods
 end
