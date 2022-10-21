75fbf7a1c04f0f53c7b22ea2c945413959934283
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-10-21 10:38:02 +0300

    Fix registration bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'recipes#index'
-  devise_for :users, controllers: { registrations: "registration" }
+  devise_for :users, controllers: { registrations: "registrations" }
 
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipe_foods, only: [:new,:create, :destroy]
