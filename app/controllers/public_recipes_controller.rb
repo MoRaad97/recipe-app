977a052dffc004701804507a0ca520bc6ce5c9f7
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-10-22 12:04:36 +0300

    Add public show page

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -2,4 +2,8 @@ class PublicRecipesController < ApplicationController
     def index
       @recipes = Recipe.includes(:user).where(public: true)
     end
+    def show
+      @recipe, = Recipe.where(id: params[:id], public: true)
+    end
+
 end
