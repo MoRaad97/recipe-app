9dd5353dcb593346908d878e8bf90b26fe4bdeec
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-10-22 11:54:21 +0100

    Fix linter error

--- spec/requests/public_recipe_spec.rb
+++ spec/requests/public_recipe_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Public_Recipe', type: :request do
-  before(:example) { get "/"}
+  before(:example) { get '/' }
 
   it('status should be 200') { expect(response).to have_http_status(200) }
 
